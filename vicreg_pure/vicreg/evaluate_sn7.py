cbf7d49393fa5ecc3dcef20548a7080e45afe8ef
Ata Soyuer <bartusoyuer@gmail.com>
2022-05-12 16:37:28 +0200

    Initial commit of Supervised and VICReg

---
+++ vicreg_pure/vicreg/evaluate_sn7.py
@@ -0,0 +1,583 @@
+from pathlib import Path
+import argparse
+import json
+import os
+import random
+import signal
+import sys
+import time
+import urllib
+
+from torch import nn, optim
+from torchvision import datasets, transforms
+import torch
+torch.cuda.empty_cache()
+
+import resnet
+
+import numpy as np
+from PIL import Image
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+import json
+
+import glob
+from geopy.geocoders import Nominatim
+import re
+import pandas as pd
+from torchvision.io import read_image
+from sklearn.preprocessing import LabelEncoder
+from torch.utils.tensorboard import SummaryWriter
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
+import math
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True   #OTHERWISE TRUNCATED IMAGE FILE ERROR SOMEWHERE IN ENUMERATE(DATALOADER)!!!!
+
+import resnet
+
+import torchvision
+
+class SN7Dataset(Dataset):
+    
+    def __init__(self, transform=None, train=True):
+        
+        if train == True:
+            self.img_ids_labels = pd.read_csv("ids_and_labels_train_subset.csv", header = None) 
+        else:
+            self.img_ids_labels = pd.read_csv("../../ids_and_labels_val_repeated.csv", header = None)
+            
+        self.img_dir = '/local/home/stuff/datasets/Challenge_7/train'
+        self.transform = transform
+        
+    def __getitem__(self, idx):
+        #print(self.img_ids_labels.iloc[idx, 0])
+        image_id, patch_id = self.img_ids_labels.iloc[idx, 0].split("!")[1],self.img_ids_labels.iloc[idx, 0].split("!")[0]
+        pattern = "mosaic_(.*?).tif"
+        location_id = re.search(pattern, image_id).group(1)
+        #print(image_id)
+        #print(location_id)
+        img_path = os.path.join(self.img_dir, location_id, 'images', image_id )
+        image = torchvision.transforms.ToTensor()(Image.open(img_path))[0:3,:,:]  #TAKE RGB CHANNELS ONLY FOR RESNET COMPATIBILITY!!!
+        
+        image_padded = torch.nn.functional.pad(image, pad=(0, 1024 - image.shape[2], 0, 1024 - image.shape[1]))
+        patches = image_padded.unfold(1, 256, 256).unfold(2, 256, 256)
+        patches = patches.reshape(3, -1, 256, 256)
+        patches = patches.permute(1,0,2,3)
+        
+        image = patches[int(patch_id)]
+        
+        label = torch.from_numpy(np.asarray(self.img_ids_labels.iloc[idx, 3])).type(torch.LongTensor)
+        
+        #patches = patches.view(-1, 3, 256, 256)
+        #print(patches.shape)
+        #label = label.flatten()
+        #label = label.type(torch.LongTensor)
+        
+        
+        image = self.transform(image)
+                
+            
+        #if self.target_transform:
+            #label = self.target_transform(label)
+            
+        
+        return image, label
+        
+    def __len__(self):
+        
+        return len(self.img_ids_labels)
+
+class VICReg(nn.Module):
+    def __init__(self, args):
+        super().__init__()
+        self.args = args
+        self.num_features = int(args.mlp.split("-")[-1])
+        self.backbone, self.embedding = resnet.__dict__[args.arch](
+            zero_init_residual=True
+        )
+        self.projector = Projector(args, self.embedding)
+
+    def forward(self, x, y):
+        x = self.projector(self.backbone(x))
+        y = self.projector(self.backbone(y))
+
+        repr_loss = F.mse_loss(x, y)
+
+        x = torch.cat(FullGatherLayer.apply(x), dim=0)
+        y = torch.cat(FullGatherLayer.apply(y), dim=0)
+        x = x - x.mean(dim=0)
+        y = y - y.mean(dim=0)
+
+        std_x = torch.sqrt(x.var(dim=0) + 0.0001)
+        std_y = torch.sqrt(y.var(dim=0) + 0.0001)
+        std_loss = torch.mean(F.relu(1 - std_x)) / 2 + torch.mean(F.relu(1 - std_y)) / 2
+
+        cov_x = (x.T @ x) / (self.args.batch_size - 1)
+        cov_y = (y.T @ y) / (self.args.batch_size - 1)
+        cov_loss = off_diagonal(cov_x).pow_(2).sum().div(
+            self.num_features
+        ) + off_diagonal(cov_y).pow_(2).sum().div(self.num_features)
+
+        loss = (
+            self.args.sim_coeff * repr_loss
+            + self.args.std_coeff * std_loss
+            + self.args.cov_coeff * cov_loss
+        )
+        return loss
+
+
+def Projector(args, embedding):
+    mlp_spec = f"{embedding}-{args.mlp}"
+    layers = []
+    f = list(map(int, mlp_spec.split("-")))
+    for i in range(len(f) - 2):
+        layers.append(nn.Linear(f[i], f[i + 1]))
+        layers.append(nn.BatchNorm1d(f[i + 1]))
+        layers.append(nn.ReLU(True))
+    layers.append(nn.Linear(f[-2], f[-1], bias=False))
+    return nn.Sequential(*layers)
+
+
+def exclude_bias_and_norm(p):
+    return p.ndim == 1
+
+
+def off_diagonal(x):
+    n, m = x.shape
+    assert n == m
+    return x.flatten()[:-1].view(n - 1, n + 1)[:, 1:].flatten()
+
+
+class LARS(optim.Optimizer):
+    def __init__(
+        self,
+        params,
+        lr,
+        weight_decay=0,
+        momentum=0.9,
+        eta=0.001,
+        weight_decay_filter=None,
+        lars_adaptation_filter=None,
+    ):
+        defaults = dict(
+            lr=lr,
+            weight_decay=weight_decay,
+            momentum=momentum,
+            eta=eta,
+            weight_decay_filter=weight_decay_filter,
+            lars_adaptation_filter=lars_adaptation_filter,
+        )
+        super().__init__(params, defaults)
+
+    @torch.no_grad()
+    def step(self):
+        for g in self.param_groups:
+            for p in g["params"]:
+                dp = p.grad
+
+                if dp is None:
+                    continue
+
+                if g["weight_decay_filter"] is None or not g["weight_decay_filter"](p):
+                    dp = dp.add(p, alpha=g["weight_decay"])
+
+                if g["lars_adaptation_filter"] is None or not g[
+                    "lars_adaptation_filter"
+                ](p):
+                    param_norm = torch.norm(p)
+                    update_norm = torch.norm(dp)
+                    one = torch.ones_like(param_norm)
+                    q = torch.where(
+                        param_norm > 0.0,
+                        torch.where(
+                            update_norm > 0, (g["eta"] * param_norm / update_norm), one
+                        ),
+                        one,
+                    )
+                    dp = dp.mul(q)
+
+                param_state = self.state[p]
+                if "mu" not in param_state:
+                    param_state["mu"] = torch.zeros_like(p)
+                mu = param_state["mu"]
+                mu.mul_(g["momentum"]).add_(dp)
+
+                p.add_(mu, alpha=-g["lr"])
+
+
+def batch_all_gather(x):
+    x_list = FullGatherLayer.apply(x)
+    return torch.cat(x_list, dim=0)
+
+
+class FullGatherLayer(torch.autograd.Function):
+    """
+    Gather tensors from all process and support backward propagation
+    for the gradients across processes.
+    """
+
+    @staticmethod
+    def forward(ctx, x):
+        output = [torch.zeros_like(x) for _ in range(dist.get_world_size())]
+        dist.all_gather(output, x)
+        return tuple(output)
+
+    @staticmethod
+    def backward(ctx, *grads):
+        all_gradients = torch.stack(grads)
+        dist.all_reduce(all_gradients)
+        return all_gradients[dist.get_rank()]
+
+
+def handle_sigusr1(signum, frame):
+    os.system(f'scontrol requeue {os.environ["SLURM_JOB_ID"]}')
+    exit()
+
+
+def handle_sigterm(signum, frame):
+    pass
+
+def get_arguments():
+    parser = argparse.ArgumentParser(
+        description="Evaluate a pretrained model on ImageNet"
+    )
+
+    # Data
+    #parser.add_argument("--data-dir", type=Path, help="path to dataset")
+    parser.add_argument(
+        "--train-percent",
+        default=100,
+        type=int,
+        choices=(100, 10, 1),
+        help="size of traing set in percent",
+    )
+
+    # Checkpoint
+    parser.add_argument("--pretrained", type=Path, help="path to pretrained model")
+    parser.add_argument(
+        "--exp-dir",
+        default="./checkpoint/lincls/",
+        type=Path,
+        metavar="DIR",
+        help="path to checkpoint directory",
+    )
+    parser.add_argument(
+        "--print-freq", default=100, type=int, metavar="N", help="print frequency"
+    )
+
+    # Model
+    parser.add_argument("--arch", type=str, default="resnet50")
+
+    # Optim
+    parser.add_argument(
+        "--epochs",
+        default=100,
+        type=int,
+        metavar="N",
+        help="number of total epochs to run",
+    )
+    parser.add_argument(
+        "--batch-size", default=256, type=int, metavar="N", help="mini-batch size"
+    )
+    parser.add_argument(
+        "--lr-backbone",
+        default=0.0,
+        type=float,
+        metavar="LR",
+        help="backbone base learning rate",
+    )
+    parser.add_argument(
+        "--lr-head",
+        default=0.3,
+        type=float,
+        metavar="LR",
+        help="classifier base learning rate",
+    )
+    parser.add_argument(
+        "--weight-decay", default=1e-6, type=float, metavar="W", help="weight decay"
+    )
+    parser.add_argument(
+        "--weights",
+        default="freeze",
+        type=str,
+        choices=("finetune", "freeze"),
+        help="finetune or freeze resnet weights",
+    )
+
+    # Running
+    parser.add_argument(
+        "--workers",
+        default=8,
+        type=int,
+        metavar="N",
+        help="number of data loader workers",
+    )
+
+    return parser
+
+
+def main():
+    parser = get_arguments()
+    args = parser.parse_args()
+    if args.train_percent in {1, 10}:
+        args.train_files = urllib.request.urlopen(
+            f"https://raw.githubusercontent.com/google-research/simclr/master/imagenet_subsets/{args.train_percent}percent.txt"
+        ).readlines()
+    args.ngpus_per_node = torch.cuda.device_count()
+    if "SLURM_JOB_ID" in os.environ:
+        signal.signal(signal.SIGUSR1, handle_sigusr1)
+        signal.signal(signal.SIGTERM, handle_sigterm)
+    # single-node distributed training
+    args.rank = 0
+    args.dist_url = f"tcp://localhost:{random.randrange(49152, 65535)}"
+    args.world_size = args.ngpus_per_node
+    torch.multiprocessing.spawn(main_worker, (args,), args.ngpus_per_node)
+
+
+def main_worker(gpu, args):
+    
+    print('main_worker')
+    args.rank += gpu
+    torch.distributed.init_process_group(
+        backend="nccl",
+        init_method=args.dist_url,
+        world_size=args.world_size,
+        rank=args.rank,
+    )
+
+    if args.rank == 0:
+        args.exp_dir.mkdir(parents=True, exist_ok=True)
+        stats_file = open(args.exp_dir / "stats.txt", "a", buffering=1)
+        print(" ".join(sys.argv))
+        print(" ".join(sys.argv), file=stats_file)
+
+    torch.cuda.set_device(gpu)
+    torch.backends.cudnn.benchmark = True
+
+    backbone, embedding = resnet.__dict__[args.arch](zero_init_residual=True)
+    state_dict = torch.load(args.pretrained, map_location="cpu")
+    if "model" in state_dict:
+        state_dict = state_dict["model"]
+        state_dict = {
+            key.replace("module.backbone.", ""): value
+            for (key, value) in state_dict.items()
+        }
+    backbone.load_state_dict(state_dict, strict=False)
+
+    head = nn.Linear(embedding, 31)
+    head.weight.data.normal_(mean=0.0, std=0.01)
+    head.bias.data.zero_()
+    model = nn.Sequential(backbone, head)
+    model.cuda(gpu)
+
+    if args.weights == "freeze":
+        backbone.requires_grad_(False)
+        head.requires_grad_(True)
+    model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[gpu])
+
+    criterion = nn.CrossEntropyLoss().cuda(gpu)
+
+    param_groups = [dict(params=head.parameters(), lr=args.lr_head)]
+    if args.weights == "finetune":
+        param_groups.append(dict(params=backbone.parameters(), lr=args.lr_backbone))
+    optimizer = optim.SGD(param_groups, 0, momentum=0.9, weight_decay=args.weight_decay)
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, args.epochs)
+
+    # automatically resume from checkpoint if it exists
+    #if (args.exp_dir / "checkpoint.pth").is_file():
+        #ckpt = torch.load(args.exp_dir / "checkpoint.pth", map_location="cpu")
+        #start_epoch = ckpt["epoch"]
+        #best_acc = ckpt["best_acc"]
+        #model.load_state_dict(ckpt["model"])
+        #optimizer.load_state_dict(ckpt["optimizer"])
+        #scheduler.load_state_dict(ckpt["scheduler"])
+    #else:
+    start_epoch = 0
+    best_acc = argparse.Namespace(top1=0, top5=0)
+
+    # Data loading code
+    
+    ids_and_labels_train = pd.read_csv('../../ids_and_labels_train_repeated.csv',delimiter=',',header=1)
+    ids_and_labels_val = pd.read_csv('../../ids_and_labels_val_repeated.csv',delimiter=',',header=1)
+    
+    ids_and_labels_train_arr = np.array(ids_and_labels_train.values)
+    ids_and_labels_val_arr = np.array(ids_and_labels_val.values)
+    
+    num_rows = ids_and_labels_train_arr.shape[0]
+    random_indices = np.random.choice(num_rows, size=int(np.round(args.train_percent*num_rows/100.0)), replace=False)
+    
+    
+    selected_training_samples = ids_and_labels_train_arr[random_indices,:]
+    
+    np.savetxt("ids_and_labels_train_subset.csv", selected_training_samples, delimiter=",", fmt='%s')
+    
+    normalize = transforms.Normalize(
+        mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+    )
+
+    train_transforms = transforms.Compose(
+            [
+                transforms.RandomResizedCrop(224),
+                transforms.RandomHorizontalFlip(),
+                #transforms.ToTensor(),
+                normalize,
+            ]
+        )
+    
+    val_transforms  = transforms.Compose(
+            [
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                #transforms.ToTensor(),
+                normalize,
+            ]
+        )
+
+    train_dataset = SN7Dataset(train=True, transform = train_transforms)
+    val_dataset = SN7Dataset(train=False, transform = val_transforms)
+    
+    print('TrainDatasetSize:', train_dataset.__len__())
+    print('ValDatasetSize:', val_dataset.__len__())
+
+    train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
+    kwargs = dict(
+        batch_size=args.batch_size // args.world_size,
+        num_workers=args.workers,
+        pin_memory=True,
+    )
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, sampler=train_sampler, **kwargs
+    )
+    val_loader = torch.utils.data.DataLoader(val_dataset, **kwargs)
+    
+    print('Trainloader size:', len(train_loader))
+    print('Valloader size:', len(val_loader))
+
+    start_time = time.time()
+    for epoch in range(start_epoch, args.epochs):
+        # train
+        if args.weights == "finetune":
+            model.train()
+        elif args.weights == "freeze":
+            model.eval()
+        else:
+            assert False
+        train_sampler.set_epoch(epoch)
+        for step, (images, target) in enumerate(
+            train_loader, start=epoch * len(train_loader)
+        ):
+            output = model(images.cuda(gpu, non_blocking=True))
+            loss = criterion(output, target.cuda(gpu, non_blocking=True))
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            if step % args.print_freq == 0:
+                torch.distributed.reduce(loss.div_(args.world_size), 0)
+                if args.rank == 0:
+                    pg = optimizer.param_groups
+                    lr_head = pg[0]["lr"]
+                    lr_backbone = pg[1]["lr"] if len(pg) == 2 else 0
+                    stats = dict(
+                        epoch=epoch,
+                        step=step,
+                        lr_backbone=lr_backbone,
+                        lr_head=lr_head,
+                        loss=loss.item(),
+                        time=int(time.time() - start_time),
+                    )
+                    print(json.dumps(stats))
+                    print(json.dumps(stats), file=stats_file)
+
+        # evaluate
+        model.eval()
+        if args.rank == 0:
+            top1 = AverageMeter("Acc@1")
+            top5 = AverageMeter("Acc@5")
+            with torch.no_grad():
+                for images, target in val_loader:
+                    output = model(images.cuda(gpu, non_blocking=True))
+                    acc1, acc5 = accuracy(
+                        output, target.cuda(gpu, non_blocking=True), topk=(1, 5)
+                    )
+                    top1.update(acc1[0].item(), images.size(0))
+                    top5.update(acc5[0].item(), images.size(0))
+            best_acc.top1 = max(best_acc.top1, top1.avg)
+            best_acc.top5 = max(best_acc.top5, top5.avg)
+            stats = dict(
+                epoch=epoch,
+                acc1=top1.avg,
+                acc5=top5.avg,
+                best_acc1=best_acc.top1,
+                best_acc5=best_acc.top5,
+            )
+            print(json.dumps(stats))
+            print(json.dumps(stats), file=stats_file)
+
+        scheduler.step()
+        if args.rank == 0:
+            state = dict(
+                epoch=epoch + 1,
+                best_acc=best_acc,
+                model=model.state_dict(),
+                optimizer=optimizer.state_dict(),
+                scheduler=scheduler.state_dict(),
+            )
+            torch.save(state, args.exp_dir / "checkpoint.pth")
+
+
+def handle_sigusr1(signum, frame):
+    os.system(f'scontrol requeue {os.getenv("SLURM_JOB_ID")}')
+    exit()
+
+
+def handle_sigterm(signum, frame):
+    pass
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self, name, fmt=":f"):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = "{name} {val" + self.fmt + "} ({avg" + self.fmt + "})"
+        return fmtstr.format(**self.__dict__)
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
+
+
+if __name__ == "__main__":
+    print('main')
+    main()
